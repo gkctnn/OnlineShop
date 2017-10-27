a0226e55a051f32b26b39146c7ac41b4ade9247a
gkctnn <zenginalgokcenur@gmail.com>
2017-10-27 17:05:22 +0300

    ModelForm

--- products/forms.py
+++ products/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Product
 
 PUBLISH_CHOISE = {
     # ('',""),
@@ -21,6 +22,45 @@ class ProductAddForm(forms.Form):
         ))
     price = forms.DecimalField()
     publish = forms.ChoiceField(widget=forms.RadioSelect,choices=PUBLISH_CHOISE,required=False)
+
+    def clean_price(self):
+        price = self.cleaned_data.get('price')
+        if price <= 1.00:
+            raise forms.ValidationError('Price must be greater than $1.00')
+        elif price >= 99.99:
+            raise forms.ValidationError('Price must be less than $100.00')
+        else:
+            return price
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        if len(title) > 3:
+            return title
+        else:
+            raise forms.ValidationError('Title must be greater than 3 characters long')
+
+class ProductModelForm(forms.ModelForm):
+    publish = forms.ChoiceField(widget=forms.RadioSelect,choices=PUBLISH_CHOISE,required=False)
+
+    class Meta:
+        model = Product
+        fields = [
+            "title",
+            "description",
+            "price",
+        ]
+        widgets = {
+        "description":forms.Textarea(
+            attrs={
+                "placeholder":"New Description"
+                }
+            ),
+        "title":forms.TextInput(
+            attrs={
+                "placeholder":"Title"
+                }
+            ),
+            }
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
--- products/views.py
+++ products/views.py
@@ -1,23 +1,15 @@
 from django.shortcuts import render,get_object_or_404
 from django.http import Http404
 from .models import Product
-from .forms import ProductAddForm
+from .forms import ProductAddForm,ProductModelForm
 # Create your views here.
 
 def create_view(request):
-    # FORM
-    form = ProductAddForm(request.POST or None)
+    form = ProductModelForm(request.POST or None)
     if form.is_valid():
-        data = form.cleaned_data
-        title = data.get('title')
-        description = data.get('description')
-        price = data.get('price')
-        # new_object = Product.objects.create(title=title,description=description,price=price)
-        new_object = Product()
-        new_object.title = title
-        new_object.description = description
-        new_object.price = price
-        new_object.save()
+        instance = form.save(commit=False)
+        instance.sale_price = instance.price
+        instance.save()
     template="create_view.html"
     context={
     'form':form,
